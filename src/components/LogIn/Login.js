c67b32007f423667406494f949b9d5d4ea77c093
Bhawani Shankar <96884049+cryo5551@users.noreply.github.com>
2026-05-27 12:14:47 +0530

    Refactor Login component to use onClick for login
    Updated the Login component to use onClick for login action.

--- src/components/LogIn/Login.js
+++ src/components/LogIn/Login.js
@@ -15,3 +15,19 @@ const Login = () => {
 
 export default Login
 */
+
+import React from 'react';
+import { loginUrl } from '../../spotify';
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png" alt="spotify" />
+      {/* We swapped href for onClick, and added a pointer cursor so it still looks like a link */}
+      <a onClick={loginUrl} style={{ cursor: 'pointer' }}>LOGIN WITH SPOTIFY</a>
+    </div>
+  )
+}
+
+export default Login;
